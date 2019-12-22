8bf82da12417705dcfe331bf0fe16f01e67fbacc
封毅 <55172101+fygod39@users.noreply.github.com>
2019-12-22 21:50:13 +0800

    第十三周作业
    第十三周作业 超级素数

---
+++ 第十三周作业.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+bool isPrime(int n)  
+{  
+    for(int i = 2; i <= sqrt(n); ++i)  
+    {  
+        if(n%i == 0)  
+            return false;  
+    }  
+    return true;  
+}
+
+int main()
+{
+    int maxprime = 0, count = 0;
+    
+    for(int i = 101; i < 10001; i+=2)
+    {
+        int  temp = i;
+        bool flag = true;
+        while(temp)
+        {
+            if(isPrime(temp))
+            {
+                temp /= 10;
+                continue;
+            }
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            count++;
+            maxprime = i;
+            printf("%4d ", i);
+            if(count%5 == 0)
+                putchar('\n');
+        }
+    }
+    return 0;
+}
